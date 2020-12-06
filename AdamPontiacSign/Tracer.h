fe037e2cbc40dd9e0ac4f085f1c40cf544f3caa7
dgaudet <dean@deangaudet.com>
2020-12-05 19:27:04 -0700

    - Tracer.h - get numRuns working properly, and some clean up

--- AdamPontiacSign/Tracer.h
+++ AdamPontiacSign/Tracer.h
@@ -2,15 +2,15 @@
 
 class Tracer {
   public:
-    Tracer(int numLedsInput, uint8_t numRuns) {
+    Tracer(int numLedsInput, uint8_t numRunsInput) {
       numLeds = numLedsInput;
+      numRuns = numRunsInput;
     };
 
     void runPattern(CRGB ledStrip[]);
   private:
     uint8_t numRuns;
-    bool upDown = false;
-    uint8_t waveSpeedBPM = 10;
+    uint8_t waveSpeedBPM = 20;
     int numLeds;
     uint8_t runCounter = 0;
 };
@@ -18,7 +18,6 @@ class Tracer {
 void Tracer::runPattern(CRGB ledStrip[]) {
   // this one uses a wave where it goes from the start to the end of the strip
   // then back the opposite direction but has a nice tail and fade out effect
-  uint8_t waveSpeedBPM = 20;
   uint8_t sinBeat1 = beatsin8(waveSpeedBPM, 0, numLeds -1, 0, 0);
   uint8_t sinBeat2 = sinBeat1 - 1;
   ledStrip[sinBeat1] = redColor;
@@ -32,8 +31,7 @@ void Tracer::runPattern(CRGB ledStrip[]) {
     runCounter++;
     Serial.print("adding in tracer");
     Serial.print("\n");
-    if (runCounter = numRuns) {
-      fill_solid(ledStrip, numLeds, CRGB::Black); //set all ledStrip to black once we get to the end
+    if (runCounter == numRuns) {
       finishedPattern();
     }
   }
