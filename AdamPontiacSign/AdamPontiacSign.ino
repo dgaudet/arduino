37363db7a2dced8bf549c7ed9628ee0b3486c3b3
dgaudet <dean@deangaudet.com>
2020-12-06 11:01:14 -0700

    - AdamPontiacSign - removing some global varialbes, and passing the finishedPattern and color to each animiation class

--- AdamPontiacSign/AdamPontiacSign.ino
+++ AdamPontiacSign/AdamPontiacSign.ino
@@ -1,10 +1,14 @@
 #include <FastLED.h>
+#include "Chaser.h"
+#include "Tracer.h"
+#include "TwoSidedOnAndOff.h"
+#include "ChaserUsingSawTooth.h"
+#include "FadeOffThenOn.h"
 
 // bugs/new features
 // when starting each new function, the starting position might not be correct
 // chaser stops randomly, not at the end of a full run, and starts randomly
 // tracer starts randomly
-
 
 #define NUM_LEDS 147
 #define DATA_PIN 2
@@ -12,26 +16,8 @@ CRGB leds[NUM_LEDS];
 
 bool isRunning = false;
 uint8_t functionRunCounter = 0;
-
 CHSV redColor = CHSV(0, 255, 255); // bright red
-CHSV blackColor = CHSV(0, 0, 0);
 
-typedef void (*fn)();
-static fn animationFunctions[5];
-
-void finishedPattern() {
-  fill_solid(leds, NUM_LEDS, CRGB::Black); //set all ledStrip to black once we get to the end
-  FastLED.show();
-  nextPattern();
-}
-
-#include "Chaser.h"
-#include "Tracer.h"
-#include "TwoSidedOnAndOff.h"
-#include "ChaserUsingSawTooth.h"
-#include "FadeOffThenOn.h"
-
-uint8_t chaserBeat = 0;
 void setup() {
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
   Serial.begin(9600);
@@ -56,10 +42,12 @@ void loop() {
       runChaser(2);
       break;
   }
+}
 
-  EVERY_N_MILLISECONDS(1000) {
-    Serial.print(functionRunCounter);
-  }
+void finishedPattern() {
+  fill_solid(leds, NUM_LEDS, CRGB::Black); //set all ledStrip to black once we get to the end
+  FastLED.show();
+  nextPattern();
 }
 
 void nextPattern() {
@@ -76,38 +64,38 @@ void runTwoSidedOnAndOff(uint8_t numRuns) {
   Serial.print("run two sided on and off");
   Serial.print("\n");
   isRunning = true;
-  TwoSidedOnAndOff twoSidedOnAndOff = TwoSidedOnAndOff(NUM_LEDS, numRuns);
-  while(isRunning) twoSidedOnAndOff.runPattern(leds);
+  TwoSidedOnAndOff twoSidedOnAndOff = TwoSidedOnAndOff(NUM_LEDS, numRuns, finishedPattern);
+  while(isRunning) twoSidedOnAndOff.runPattern(leds, redColor);
 }
 
 void runTracer(uint8_t numRuns) {
   Serial.print("run tracer");
   Serial.print("\n");
   isRunning = true;
-  Tracer tracer = Tracer(NUM_LEDS, numRuns);
-  while(isRunning) tracer.runPattern(leds);
+  Tracer tracer = Tracer(NUM_LEDS, numRuns, finishedPattern);
+  while(isRunning) tracer.runPattern(leds, redColor);
 }
 
 void runChaser(uint8_t numRuns) {
   Serial.print("run chaser");
   Serial.print("\n");
   isRunning = true;
-  Chaser chaser = Chaser(NUM_LEDS, numRuns);
-  while(isRunning) chaser.runPattern(leds);
+  Chaser chaser = Chaser(NUM_LEDS, numRuns, finishedPattern);
+  while(isRunning) chaser.runPattern(leds, redColor);
 }
 
 void runChaserUsingSawTooth(uint8_t numRuns) {
   Serial.print("run ChaserUsingSawTooth");
   Serial.print("\n");
   isRunning = true;
-  ChaserUsingSawTooth chaser = ChaserUsingSawTooth(NUM_LEDS, numRuns);
-  while(isRunning) chaser.runPattern(leds);
+  ChaserUsingSawTooth chaser = ChaserUsingSawTooth(NUM_LEDS, numRuns, finishedPattern);
+  while(isRunning) chaser.runPattern(leds, redColor);
 }
 
 void runFadeOffThenOn(uint8_t numRuns) {
   Serial.print("run FadeOffThenOn");
   Serial.print("\n");
   isRunning = true;
-  FadeOffThenOn fader = FadeOffThenOn(NUM_LEDS, numRuns);
-  while(isRunning) fader.runPattern(leds);
+  FadeOffThenOn fader = FadeOffThenOn(NUM_LEDS, numRuns, finishedPattern);
+  while(isRunning) fader.runPattern(leds, redColor);
 }
