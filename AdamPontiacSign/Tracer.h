60fa85742889db2ec5d7fae4bfd9d7b2f3a3f6e4
dgaudet <dean@deangaudet.com>
2020-12-28 16:39:03 -0700

    - Tracer - no longer using wave, which ensures the animation starts and finishes at the correct led

--- AdamPontiacSign/Tracer.h
+++ AdamPontiacSign/Tracer.h
@@ -12,34 +12,46 @@ class Tracer {
       finishedPatternCB = finishedCallBack;
     };
 
-    void runPattern(CRGB ledStrip[], CHSV color);
+    void runPattern(CRGB ledStrip[], CHSV color) {
+      // this one uses a wave where it goes from the start to the end of the strip
+      // then back the opposite direction but has a nice tail and fade out effect
+      EVERY_N_MILLISECONDS(7) {
+        if (upDown) {
+          lightCounter++;
+        } else {
+          lightCounter--;
+        }
+
+        if (lightCounter >= numLeds) {
+          upDown = false;
+          runCounter++;
+          Serial.print("adding in tracer");
+          Serial.print("\n");
+        }
+        if (lightCounter <= 0) {
+          upDown = true;
+          runCounter++;
+          Serial.print("adding in tracer");
+          Serial.print("\n");
+        }
+      }
+
+      if (runCounter == numRuns*2) {
+        finishedPatternCB();
+      }
+
+      ledStrip[lightCounter] = color;
+      fadeToBlackBy(ledStrip, numLeds, 20);
+      FastLED.show();
+    }
+
   private:
     void (*finishedPatternCB)();
-    
+
+    boolean upDown = true;
     uint8_t numRuns;
     uint8_t waveSpeedBPM = 20;
+    int lightCounter = 0;
     int numLeds;
     uint8_t runCounter = 0;
 };
-
-void Tracer::runPattern(CRGB ledStrip[], CHSV color) {
-  // this one uses a wave where it goes from the start to the end of the strip
-  // then back the opposite direction but has a nice tail and fade out effect
-  uint8_t sinBeat1 = beatsin8(waveSpeedBPM, 0, numLeds -1, 0, 0);
-  uint8_t sinBeat2 = sinBeat1 - 1;
-  ledStrip[sinBeat1] = color;
-  ledStrip[sinBeat2] = color;
-  
-  fadeToBlackBy(ledStrip, numLeds, 20);
-  FastLED.show();
-
-  int timeForOneFullWave = (60/waveSpeedBPM)*1000;
-  EVERY_N_MILLISECONDS(timeForOneFullWave*2) {
-    runCounter++;
-    Serial.print("adding in tracer");
-    Serial.print("\n");
-    if (runCounter == numRuns) {
-      finishedPatternCB();
-    }
-  }
-}
