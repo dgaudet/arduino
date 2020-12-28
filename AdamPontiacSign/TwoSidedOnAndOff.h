dfbb3d60a28f31847761be2d260f8139753af39c
dgaudet <dean@deangaudet.com>
2020-12-28 16:02:29 -0700

    - TwoSidedOnAndOff - change the function to be directly in the class

--- AdamPontiacSign/TwoSidedOnAndOff.h
+++ AdamPontiacSign/TwoSidedOnAndOff.h
@@ -12,7 +12,40 @@ class TwoSidedOnAndOff {
       finishedPatternCB = finishedCallBack;
     };
 
-    void runPattern(CRGB ledStrip[], CHSV color);
+    void runPattern(CRGB ledStrip[], CHSV color) {
+      CHSV currentColor = color;
+      EVERY_N_MILLISECONDS(50) {
+        if (brightness == 0) {
+          fill_solid(ledStrip, numLeds, blackColor);
+          FastLED.show();
+        }
+        if (upDown){
+          currentColor = color;
+          ledStrip[brightness] = currentColor;
+          ledStrip[numLeds - brightness -1] = currentColor;
+          brightness++;
+        } else {
+          currentColor = blackColor;
+          ledStrip[brightness] = currentColor;
+          ledStrip[numLeds - brightness -1] = currentColor;
+          brightness--;
+        }
+        if (brightness > numLeds/2){
+          upDown = false;
+        }
+        if (brightness < 1) {
+          upDown = true;
+          runCounter++;   //increment functionRunCounter everytime we make it through one full cycle
+          Serial.print("adding in twoSidedOnThenOff");
+          Serial.print("\n");
+          if (runCounter == numRuns) {
+            finishedPatternCB();
+          }
+        }
+        FastLED.show();
+      }
+    }
+    
   private:
     void (*finishedPatternCB)();
     
@@ -23,37 +56,3 @@ class TwoSidedOnAndOff {
     uint8_t brightness = 0;
     CHSV blackColor = CHSV(0, 0, 0);
 };
-
-void TwoSidedOnAndOff::runPattern(CRGB ledStrip[], CHSV color) {
-  CHSV currentColor = color;
-  EVERY_N_MILLISECONDS(50) {
-    if (brightness == 0) {
-      fill_solid(ledStrip, numLeds, blackColor);
-      FastLED.show();
-    }
-    if (upDown){
-      currentColor = color;
-      ledStrip[brightness] = currentColor;
-      ledStrip[numLeds - brightness -1] = currentColor;
-      brightness++;
-    } else {
-      currentColor = blackColor;
-      ledStrip[brightness] = currentColor;
-      ledStrip[numLeds - brightness -1] = currentColor;
-      brightness--;
-    }
-    if (brightness > numLeds/2){
-      upDown = false;
-    }
-    if (brightness < 1) {
-      upDown = true;
-      runCounter++;   //increment functionRunCounter everytime we make it through one full cycle
-      Serial.print("adding in twoSidedOnThenOff");
-      Serial.print("\n");
-      if (runCounter == numRuns) {
-        finishedPatternCB();
-      }
-    }
-    FastLED.show();
-  }
-}
