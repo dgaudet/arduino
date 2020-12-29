958a132847576bb74228052fc093b4363f43c9d9
dgaudet <dean@deangaudet.com>
2020-12-28 17:51:03 -0700

    - Chaser - fixing the first led to ensure it's the correct color, and on at the right times

--- AdamPontiacSign/Chaser.h
+++ AdamPontiacSign/Chaser.h
@@ -15,9 +15,24 @@ class Chaser {
     void runPattern(CRGB ledStrip[], CHSV ledColor) {
       // this function will light up all leds one at a time from the start to the end
       // then on the way back it will turn each led off one at a time to the start
-      CRGB color;
+      CRGB color = ledColor;
+      // need to turn on the first led otherwise it's always off
+      ledStrip[0] = color;
 
       EVERY_N_MILLISECONDS(25) {
+        if (lightCounter >= numLeds) {
+          upDown = false;
+          runCounter++;
+          Serial.print("adding in chaser");
+          Serial.print("\n");
+        }
+        if (lightCounter < 0) {
+          upDown = true;
+          runCounter++;
+          Serial.print("adding in chaser");
+          Serial.print("\n");
+        }
+        
         if (upDown) {
           lightCounter++;
           color = ledColor;
@@ -26,26 +41,13 @@ class Chaser {
           color = CRGB::Black;
         }
 
-        if (lightCounter >= numLeds) {
-          upDown = false;
-          runCounter++;
-          Serial.print("adding in chaser");
-          Serial.print("\n");
-        }
-        if (lightCounter <= 0) {
-          upDown = true;
-          runCounter++;
-          Serial.print("adding in chaser");
-          Serial.print("\n");
-        }
+        ledStrip[lightCounter] = color;
+        FastLED.show();
       }
 
       if (runCounter == numRuns*2) {
         finishedPatternCB();
       }
-
-      ledStrip[lightCounter] = color;
-      FastLED.show();
     }
 
   private:
