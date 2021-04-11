19f2c9103990f53bf8993c85c2062833d5a7a07e
dgaudet <dean@deangaudet.com>
2021-04-10 20:51:37 -0600

    - FadeInAndOut - add both strips

--- EnverusSign/FadeInAndOut.h
+++ EnverusSign/FadeInAndOut.h
@@ -12,8 +12,16 @@ class FadeInAndOut {
 
     void runPattern(Adafruit_NeoPixel &strip1, Adafruit_NeoPixel &strip2) {
       fadeInAndOut(strip1, topNumLeds);
-//      fadeInAndOut(strip2, bottomNumLeds);
+      fadeInAndOut(strip2, bottomNumLeds);
     }
+
+  private:
+    void (*finishedPatternCB)();
+    bool upDown = true;
+    int brightness = 0;
+
+    int topNumLeds;
+    int bottomNumLeds;
 
     void fadeInAndOut(Adafruit_NeoPixel &strip, int numLeds) {
       Serial.print("test");
@@ -26,6 +34,7 @@ class FadeInAndOut {
         }
         
         uint32_t color = strip.Color(0, 0, 0, brightness);
+//        uint32_t color = strip.Color(0, 255, 0, 0);
         strip.fill(color, 0, numLeds);
         if (brightness > 100) {
           upDown = false;
@@ -37,12 +46,4 @@ class FadeInAndOut {
         strip.show();
       }
     }
-
-  private:
-    void (*finishedPatternCB)();
-    bool upDown = true;
-    int brightness = 0;
-
-    int topNumLeds;
-    int bottomNumLeds;
 };
