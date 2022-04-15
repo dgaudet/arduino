f80488eefd693d711c482cc255e23810086e5037
dgaudet <dean.gaudet@rseg.com>
2022-04-15 14:09:34 -0600

    - Adding some comments, remove some unused code, fixing some of the animations, and added a new one

--- ArcadeLEDs/ArcadeLEDs.ino
+++ ArcadeLEDs/ArcadeLEDs.ino
@@ -4,16 +4,17 @@ const int numLeds = 96;
 const int pin = 4;
 const int nextButton = 3;
 const int colorButton = 2;
-const int maxAnimation = 11;
+const int maxAnimation = 13;
 const int maxColor = 8;
 
-int animation = 5;
+int animation = 0; // the starting animation 
 int color = 2;
-int state = 0;
+int state = 1;    // used to determine that time is moving, each second this gets bumped
 int fadeState = 0;
-int maxState = 0;
-int changeTime = 1;
+int maxState = 100; // used to restart the state and increment the animation number when it hit this number for a given animation
+int changeTime = 1; // used to determine how much time before the state is incremented
 int blendSpeed = 2;
+int brightness = 50; // this is the highest level of brightness that the lights will go to
 
 uint8_t hue = 0;
 uint8_t blendAmount = 0;
@@ -44,23 +45,10 @@ void setup() {
   randomSeed(analogRead(0));
   
   FastLED.addLeds<WS2812, pin, GRB>(leds, numLeds);
-  FastLED.setBrightness(50);
+  FastLED.setBrightness(brightness);
   FastLED.setCorrection(UncorrectedColor);
   clear();
   update();
-
-  animations[0] = solid;//X
-  animations[1] = solidBlink;
-  animations[2] = moveOneWay;
-  animations[3] = moveTwoWayFade;
-  animations[4] = topBottomFade;
-  animations[5] = rainbow;
-  animations[6] = fadeHueSolid;
-  animations[7] = fadeHue;
-  animations[8] = RGWWave;
-  animations[9] = RGWBlink;//X
-  animations[10] = threeAreas;
-  animations[11] = closeIn;
 
   colors[0] = CRGB::Black;
   colors[1] = CRGB::Red;
@@ -71,42 +59,41 @@ void setup() {
   colors[6] = CRGB::Cyan;
   colors[7] = CRGB::White;
 
+  animations[0] = threeAreas;
+  animations[1] = moveOneWay;
+  animations[2] = rainbow;
+  animations[3] = moveTwoWayFade;
+  animations[4] = fadeHueSolid;
+  animations[5] = RGWWave;
+  animations[6] = solid;
+  animations[7] = closeIn;
+  animations[8] = closeIn;
+  animations[9] = closeIn;
+  animations[10] = closeIn;
+  animations[11] = closeIn;
+  animations[12] = closeOut;
+
   for(int i = 0; i < numLeds; i++) {
     colorIndex[i] = random8();
   }
-//  
-//  attachInterrupt(digitalPinToInterrupt(colorButton), nextColor, RISING);
-//  attachInterrupt(digitalPinToInterrupt(nextButton), nextAnimation, RISING);
+
   Serial.println("end setup");
 }
 
 void loop() {  
   Serial.println("start loop");
-
-//  EVERY_N_MILLISECONDS(10) {
-//    clear();
-//    blend(leds2, leds, output, numLeds, blendAmount);
-//    if(useLeds1) {
-//      if(blendAmount < 255) {
-//        blendAmount ++; 
-//      }
-//    } else {
-//      if(blendAmount > 0) {
-//        blendAmount --;
-//      }
-//    }
-//  }
   
   updateState();
 
-//  Serial.println(animation);
   animations[animation]();
   if(animation < maxAnimation - 1) {
-    animations[animation+1];
+    animations[animation];
   } else {
     animations[0];
   }
-  
+  Serial.print("on animation: ");
+  Serial.print(animation);
+  Serial.println();
   update();
   Serial.println("end loop");
   delay(10);
@@ -121,34 +108,22 @@ void clear() {
 }
 
 void updateState() {
-//  Serial.print("animation: ");
-//  Serial.print(animation);
-//  Serial.print("  color: ");
-//  Serial.println(color);
+  Serial.print("updating state: ");
+  Serial.print(state);
+  Serial.println();
   
   if ((millis() - prevChangeTime) > changeTime) {
     state++;
     if(state > maxState) {
-      state = 0;
+      state = 1;
       fadeState++;
       animation++;
+      if(animation >= maxAnimation) {
+        animation = 0;
+      }
     }
     prevChangeTime = millis();
   }
-}
-
-void nextAnimation() {  
-  unsigned long currentTime = millis();
-  if((currentTime - lastDebounceTimeNext) > debounceDelay) {
-     animation++;
-     useLeds1 = !useLeds1;
-      if(animation > maxAnimation) {
-        animation = 0;
-      }
-      state = 0;
-  }
-
-  lastDebounceTimeNext = currentTime;
 }
 
 void nextColor() {  
@@ -165,45 +140,59 @@ void nextColor() {
 //<animation-functions>
 
 void solid() {
+  changeTime = 1000;
+  maxState = 5;
   fill_solid(leds, numLeds, colors[color]);
 }
 
 void solidBlink() {
-  changeTime = 200;
-  maxState = 1;
-  if(state == 0) {
-    fill_solid(leds2, numLeds, colors[color]);
+  // currently not working
+  changeTime = 10;
+  maxState = 100;
+  if(state % 2 == 0) {
+    fill_solid(leds, numLeds, colors[color]);
   } else {
     clear();
+    Serial.println("---------clear---------");
   }
 }
 
 void moveOneWay() {
   changeTime = 60;
+  maxState = 300;
   uint8_t led = map(beat8(changeTime, 0), 0, 255, 0, numLeds-1);
-  leds[led] = colors[color];
+  leds[led] = colors[1];
 
   fadeToBlackBy(leds, numLeds, 10);
 }
 
 void moveTwoWayFade() {
-  changeTime = 50;
+  changeTime = 5;
+  maxState = 350;
   uint8_t sinBeat = beatsin8(changeTime, 0, numLeds - 1, 0, 0);
-  leds2[sinBeat] = colors[color];
+  leds[sinBeat] = colors[3];
 
-  fadeToBlackBy(leds2, numLeds, 10);
+  fadeToBlackBy(leds, numLeds, 10);
 }
 
 void topBottomFade() {
+  // currently not working
   changeTime = 50;
+  maxState = 100;
   uint8_t sinBeat = beatsin8(changeTime, 0, numLeds - 1, 0, 0);
-  leds[sinBeat] = colors[color];
+  leds[sinBeat] = colors[1];
 
-  fadeToBlackBy(leds, numLeds, 1);
+  fadeToBlackBy(leds, numLeds, 10);
 }
 
 void rainbow() {
-  fill_rainbow(leds2, numLeds, 0, 255 / numLeds);
+  changeTime = 50;
+  maxState = brightness + 50;
+  if(state < maxState - 50) {
+    fill_rainbow(leds, numLeds, 0, 255 / numLeds);
+  } else {
+    fadeToBlackBy(leds, numLeds, 10);
+  }
 }
 
 void fadeHueSolid() {
@@ -221,14 +210,16 @@ void fadeHue() {
 }
 
 void fadeHueFast() {
+  // this works but isn't interesting unfortunately
   changeTime = 15;
   maxState = 255;
   for(int led = 0; led < numLeds; led++) {
-    leds[led] = CHSV(state + (led*30), 255, 255);
+    leds[led] = CHSV(state + (led*60), 255, 255);
   }
 }
 
 void RGWWave() {
+  maxState = 200;
   uint8_t sinBeat1 = beatsin8(30, 0, numLeds-1, 0, 0);
   uint8_t sinBeat2 = beatsin8(30, 0, numLeds-1, 0, 85);
   uint8_t sinBeat3 = beatsin8(30, 0, numLeds-1, 0, 170);
@@ -250,10 +241,11 @@ DEFINE_GRADIENT_PALETTE(redGreenWhite_gp) {
 CRGBPalette16 RGW = redGreenWhite_gp;
 
 void RGWBlink() {
+  // currently doesn't work
   changeTime = 5;
   maxState = 1;
   for(int led = 0; led < numLeds; led++) {
-    leds2[led] = ColorFromPalette(RGW, colorIndex[led]);
+    leds[led] = ColorFromPalette(RGW, colorIndex[led]);
   }
 
   if(state == 1) {
@@ -265,8 +257,7 @@ void RGWBlink() {
 
 void threeAreas() {
   changeTime = 50;
-  maxState = 81;
-  Serial.println(state);
+  maxState = 151;
   if(state >= 80) {
     firstThird(CRGB(255, 0, 0));
     secondThird(CRGB(0, 255, 0));
@@ -290,17 +281,31 @@ void threeAreas() {
 }
 
 void closeIn() {
-//  test++;
-  changeTime = 1;
-  maxState = 22;
-  int test = state;
-//  lightUpArea(0, state, CRGB::Red);
-  leds[test] = CRGB::Red;
-//  for(int i = 0; i < numLeds; i++) {
-//    if(i == state) {
-//      leds[i] = CRGB::Red;
-//    }
-//  }
+  CRGB color = colors[animation-6];
+  changeTime = 60;
+  maxState = 70;
+  if(state <= 25) {
+    // light up left
+    leds[state-2] = color;
+    leds[numLeds - state] = color;
+    // light up right
+    leds[46+state] = color;
+    leds[47-state] = color;
+  }
+}
+
+void closeOut() {
+  CRGB color = colors[0];
+  changeTime = 40;
+  maxState = 70;
+  if(state <= 25) {
+    // light up left
+    leds[25-state] = color;
+    leds[70+state] = color;
+    // light up right
+    leds[22 + state] = color;
+    leds[72 - state] = color;
+  }
 }
 
 //</animation-functions>
